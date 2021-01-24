49fed93afa0d35963bd522a3783c28bf8669f21c
Vitor Oliveira <vbrazo@gmail.com>
2021-01-24 11:54:51 -0800

    Add Floyds cycle find algorithm explanatin

--- linked_lists/linked_list_circle.rb
+++ linked_lists/linked_list_circle.rb
@@ -7,6 +7,48 @@
 
 # Return true if there is a cycle in the linked list. Otherwise, return false
 
+# Approach 2: Floyd's Cycle Finding Algorithm
+# Intuition
+#
+# Imagine two runners running on a track at different speed.
+# What happens when the track is actually a circle?
+#
+# Algorithm
+#
+# The space complexity can be reduced to O(1) by considering two pointers at
+# different speed - a slow pointer and a fast pointer. The slow pointer moves
+# one step at a time while the fast pointer moves two steps at a time.
+#
+# If there is no cycle in the list, the fast pointer will eventually reach the end
+# and we can return false in this case.
+#
+# Now consider a cyclic list and imagine the slow and fast pointers are two runners
+# racing around a circle track. The fast runner will eventually meet the slow runner.
+# Why? Consider this case (we name it case A) - The fast runner is just one step
+# behind the slow runner. In the next iteration, they both increment one and two steps
+# respectively and meet each other.
+#
+# How about other cases? For example, we have not considered cases where the fast runner
+# is two or three steps behind the slow runner yet. This is simple, because in the next
+# or next's next iteration, this case will be reduced to case A mentioned above.
+
+# Complexity analysis
+#
+# Time complexity: O(n). Let us denote nn as the total number of nodes in the linked list.
+# To analyze its time complexity, we consider the following two cases separately.
+#
+# List has no cycle:
+# The fast pointer reaches the end first and the run time depends on the
+# list's length, which is O(n).
+#
+# List has a cycle:
+# We break down the movement of the slow pointer into two steps, the non-cyclic part
+# and the cyclic part:
+#
+# The worst case time complexity is O(N+K), which is O(n).
+#
+# Space complexity: O(1). We only use two nodes (slow and fast) so the space complexity is O(1).
+
 class ListNode
   attr_accessor :val, :next
   def initialize(val)
