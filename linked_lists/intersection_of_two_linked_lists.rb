22c1babb9ea020dd7fa1441ee9d030cac66c6f96
Vitor Oliveira <vbrazo@gmail.com>
2021-01-25 19:45:59 -0800

    Minor changes

--- linked_lists/intersection_of_two_linked_lists.rb
+++ linked_lists/intersection_of_two_linked_lists.rb
@@ -11,14 +11,6 @@
 #
 # Space complexity : O(1).
 
-# class ListNode
-#   attr_accessor :val, :next
-#   def initialize(val)
-#     @val = val
-#     @next = nil
-#   end
-# end
-
 # Approach 2: Hash Table
 # Traverse list A and store the address / reference to each node in a hash set.
 # Then check every node bi in list B: if bi appears in the hash set,
@@ -31,30 +23,38 @@
 # Space complexity: O(m) or O(n).
 
 # Approach 3: Two Pointers
-# Maintain two pointers pApA and pBpB initialized at the head of A and B,
+# Maintain two pointers pA and pB initialized at the head of A and B,
 # respectively. Then let them both traverse through the lists, one node
-# at a time. When pApA reaches the end of a list, then redirect
+# at a time. When pA reaches the end of a list, then redirect
 # it to the head of B (yes, B, that's right.); similarly when pBpB
 # reaches the end of a list, redirect it the head of A.
-# If at any point pApA meets pBpB, then pApA/pBpB is the intersection node.
+# If at any point pA meets pB, then pA/pB is the intersection node.
 # To see why the above trick would work, consider the following two lists:
 # A = {1,3,5,7,9,11} and B = {2,4,9,11}, which are intersected at node '9'.
-# Since B.length (=4) < A.length (=6), pBpB would reach the end of the
-# merged list first, because pBpB traverses exactly 2 nodes less than
-# pApA does. By redirecting pBpB to head A, and pApA to head B,
-# we now ask pBpB to travel exactly 2 more nodes than pApA would.
+# Since B.length (=4) < A.length (=6), pB would reach the end of the
+# merged list first, because pB traverses exactly 2 nodes less than
+# pApA does. By redirecting pB to head A, and pApA to head B,
+# we now ask pB to travel exactly 2 more nodes than pApA would.
 # So in the second iteration, they are guaranteed to reach the intersection
 # node at the same time. If two lists have intersection,
 # then their last nodes must be the same one.
-# So when pApA/pBpB reaches the end of a list, record the last element
+# So when pA/pB reaches the end of a list, record the last element
 # of A/B respectively. If the two last elements are not the same one,
 # then the two lists have no intersections.
 #
 # Complexity Analysis
 #
-# Time complexity : O(m+n).
+# Time complexity: O(m+n).
 #
-# Space complexity : O(1)
+# Space complexity: O(1).
+
+class ListNode
+  attr_accessor :val, :next
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+end
 
 # @param {ListNode} headA
 # @param {ListNode} headB
