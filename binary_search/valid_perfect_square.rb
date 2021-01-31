5fb5bfe686bfdf1a673ca31df3a6c8a2334d6999
Vitor Oliveira <vbrazo@gmail.com>
2021-01-30 23:33:19 -0800

    Add valid perfect square

---
+++ binary_search/valid_perfect_square.rb
@@ -0,0 +1,16 @@
+# Given a positive integer num, write a function which returns True
+# if num is a perfect square else False.
+#
+# Follow up: Do not use any built-in library function such as sqrt.
+
+# @param {Integer} num
+# @return {Boolean}
+def is_perfect_square(num)
+  
+end
+
+num = 16
+# => true
+
+num = 14
+# => false
