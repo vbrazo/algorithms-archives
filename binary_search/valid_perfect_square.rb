b96b00aed894e7af9ba3ed47cedac744946a5b97
Vitor Oliveira <vbrazo@gmail.com>
2021-01-30 23:43:39 -0800

    Use binary search to solve problem

--- binary_search/valid_perfect_square.rb
+++ binary_search/valid_perfect_square.rb
@@ -3,14 +3,34 @@
 #
 # Follow up: Do not use any built-in library function such as sqrt.
 
-# @param {Integer} num
-# @return {Boolean}
 def is_perfect_square(num)
-  
+  if num < 2
+    return true
+  end
+
+  left = 2
+  right = num
+
+  while left <= right
+    x = left + (right - left)
+    guess_squared = x * x
+    if guess_squared == num
+      return true
+    end
+    if guess_squared > num
+      right = x - 1
+    else
+      left = x + 1
+    end
+  end
+
+  return false
 end
 
 num = 16
+puts(is_perfect_square(num))
 # => true
 
 num = 14
+puts(is_perfect_square(num))
 # => false
