1775842f5e05f28f063debdec6b43327f43b0212
Vitor Oliveira <vbrazo@gmail.com>
2021-02-06 21:02:08 -0800

    Remove linked list elements

---
+++ linked_lists/remove_linked_list_elements.rb
@@ -0,0 +1,45 @@
+# Remove all elements from a linked list of integers that have value val.
+
+# Approach 1: Sentinel Node
+
+# The problem seems to be very easy if one has to delete a node in the middle:
+# Pick the node-predecessor prev of the node to delete.
+# Set its next pointer to point to the node next to the one to delete.
+
+# Algorithm
+# Initiate sentinel node as ListNode(0) and set it to be the new head: sentinel.next = head.
+# Initiate two pointers to track the current node and its predecessor: curr and prev.
+# While curr is not a null pointer:
+# Compare the value of the current node with the value to delete.
+# In the values are equal, delete the current node: prev.next = curr.next.
+# Otherwise, set predecessor to be equal to the current node.
+# Move to the next node: curr = curr.next.
+# Return sentinel.next.
+
+# Time complexity: O(N), it's one pass solution.
+# Space complexity: O(1), it's a constant space solution.
+
+def remove_elements(head, val)
+  return nil if head.nil?
+
+  sentinel = ListNode.new(0)
+  sentinel.next = head
+
+  prev = sentinel
+  current = head
+
+  while current != nil
+    if current.val == val
+        prev.next = current.next
+    else
+        prev = current
+    end
+    current = current.next
+  end
+
+  sentinel.next
+end
+
+remove_elements(head, 6)
+# Input:  1->2->6->3->4->5->6, val = 6
+# Output: 1->2->3->4->5
