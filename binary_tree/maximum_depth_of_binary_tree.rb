f2ba9f5993be4456953c9f4a22b2c4f6b4c1165f
Vitor Oliveira <vbrazo@gmail.com>
2021-02-13 13:53:22 -0800

    Add approach 3

--- binary_tree/maximum_depth_of_binary_tree.rb
+++ binary_tree/maximum_depth_of_binary_tree.rb
@@ -82,7 +82,7 @@ end
 # Time complexity: O(N), still we visit each node once and only once.
 # As one can see, this probably is not the best example to apply the tail recursion
 # technique. Because though we did gain the constant space complexity for the recursive calls,
-# we pay the price of \mathcal{O}(N)O(N) complexity to maintain the state information
+# we pay the price of O(N) complexity to maintain the state information
 # for recursive calls. This defeats the purpose of applying tail recursion.
 # However, we would like to stress on the point that tail recursion is a useful form
 # of recursion that could eliminate the space overhead incurred by the recursive function calls.
@@ -92,3 +92,50 @@ end
 # that occur within the same function.
 
 # TODO:
+
+# Approach 3: Iteration
+#
+# Intuition
+#
+# We could also convert the above recursion into iteration, with the help of the stack data
+# structure. Similar with the behaviors of the function call stack, the stack data structure
+# follows the pattern of FILO (First-In-Last-Out), i.e. the last element that is added
+# to a stack would come out first.
+# With the help of the stack data structure, one could mimic the behaviors of function call
+# stack that is involved in the recursion, to convert a recursive function to a function with
+# iteration.
+#
+# Algorithm
+#
+# The idea is to keep the next nodes to visit in a stack. Due to the FILO behavior of stack,
+# one would get the order of visit same as the one in recursion.
+# We start from a stack which contains the root node and the corresponding depth which
+# is 1. Then we proceed to the iterations: pop the current node out of the stack and
+# push the child nodes. The depth is updated at each step.
+
+# Complexity analysis
+
+# Time complexity: O(N).
+
+# Space complexity: in the worst case, the tree is completely unbalanced, e.g. each node
+# has only left child node, the recursion call would occur N times (the height of the tree),
+# therefore the storage to keep the call stack would be O(N). But in the
+# average case (the tree is balanced), the height of the tree would be log(N).
+# Therefore, the space complexity in this case would be O(log(N)).
+
+def max_depth(root)
+  node_queue = [root]
+  depth = 0
+
+  while node_queue.any?
+    depth +=1
+    breadth = node_queue.size
+    breadth.times do
+      node = node_queue.shift
+      node_queue.push(node.left) unless node.left.nil?
+      node_queue.push(node.right) unless node.right.nil?
+    end
+  end
+
+  depth
+end
