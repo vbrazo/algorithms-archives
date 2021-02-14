89265db2f766683696b295fade035dc55c6202ad
Vitor Oliveira <vbrazo@gmail.com>
2021-02-13 22:28:16 -0800

    Add approach 1 explanation

--- binary_tree/symmetric_tree.rb
+++ binary_tree/symmetric_tree.rb
@@ -16,6 +16,30 @@
 #   \  \
 #    3  3
 
+# Approach 1: Recursive
+
+# A tree is symmetric if the left subtree is a mirror reflection of the right subtree.
+# Therefore, the question is: when are two trees a mirror reflection of each other?
+# Two trees are a mirror reflection of each other if:
+# Their two roots have the same value.
+# The right subtree of each tree is a mirror reflection of the left subtree of
+# the other tree.
+
+# This is like a person looking at a mirror.
+# The reflection in the mirror has the same head, but the reflection's right arm
+# corresponds to the actual person's left arm, and vice versa.
+
+# The explanation above translates naturally to a recursive function as follows.
+
+# Complexity Analysis
+
+# Time complexity: O(n). Because we traverse the entire input tree once,
+# the total run time is O(n), where nn is the total number of nodes in the tree.
+
+# Space complexity: The number of recursive calls is bound by the height of the tree.
+# In the worst case, the tree is linear and the height is in O(n). Therefore,
+# space complexity due to recursive calls on the stack is O(n) in the worst case.
+
 # Definition for a binary tree node.
 class TreeNode
   attr_accessor :val, :left, :right
