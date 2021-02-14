765568bb3abc80a372c84df9a0689a8bdc377d10
Vitor Oliveira <vbrazo@gmail.com>
2021-02-14 01:15:06 -0800

    Add approach 2

--- arrays/third_maximum_number.rb
+++ arrays/third_maximum_number.rb
@@ -37,80 +37,125 @@ puts(third_max(nums))
 # Output: 2
 
 
-# Approach 2: Use a Set and Delete Maximums
+# Approach 2: Seen-Maximums Set
 
-# We'll work with the following example Array.
+# Intuition
+
+# In the previous approach, we deleted the maximum and second maximum so that
+# we could easily find the third maximum. We had to convert the input Array into a
+# Set so that duplicates weren't super complicated to handle.
+# Instead of deleting items though, we could instead keep a Set of maximums
+# we've already seen. Then when we are searching for a maximum, we can ignore
+# any values that are already in the seen Set.
+# This will also handle duplicates elegantly—if for example we had the input
+# set [12, 12, 4, 2, 12, 1], then the first value we'd put into the seen maximums
+# Set would be 12. Then when we find the second maximum, the algorithm knows
+# to ignore all the 12s.
 #
-# [12, 3, 8, 9, 12, 12, 7, 8, 12, 4, 3, 8, 1]
-# If there were no duplicates in the Array,
-# then a logical strategy would be as follows:
+# Complexity Analysis
 #
-# Find the maximum. Delete it.
-# Find the new maximum. Delete it.
-# Return the *new* maximum.
+# Time Complexity: O(n).
 #
-# However, the input Array we're working with could have duplicates.
-# To handle this, we can convert the input into a Set first to remove the duplicates.
+# For each of the three times we find the next maximum, we need to perform an O(n) scan.
+# Because there are only, at most, three scans the total time complexity is just O(n).
+# The Set operations are all O(1) because there are only at most 3 items in the Set.
 #
-# Converting our input Array example into a Set gives us the following:
+# Space Complexity: O(1).
 #
-# {12, 3, 8, 9, 7, 4, 1}
-# We then need to find the maximum in the Set. This can be done using a library
-# function, or if necessary, your own function that loops through the list keeping
-# track of the maximum seen so far, and then returns the maximum at the end.
+# Because seenMaximums can contain at most 3 items, the space complexity is only O(1).
+require 'set'
+
+def maximum_ignoring_seen_maximums(nums, seen_maximums)
+  maximum = nil
+
+  nums.each do |num|
+    next if seen_maximums.include? num
+
+    if maximum.nil? || num > maximum
+      maximum = num
+    end
+  end
+
+  maximum
+end
+
+def third_max(nums)
+  seen_maximums = nums.to_set
+
+  3.times do
+    current_maximum = maximum_ignoring_seen_maximums(nums, seen_maximums)
+
+    return seen_maximums.max if current_maximum.nil?
+
+    seen_maximums.add(current_maximum)
+  end
+
+  seen_maximums.min
+end
+
+nums = [3,2,1]
+print(third_max(nums))
+# Output: 1
+# Explanation: The third maximum is 1.
+
+nums = [1,2]
+print(third_max(nums))
+# Output: 2
+# Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+
+nums = [2,2,3,1]
+puts(third_max(nums))
+# Output: 1
+# Explanation: Note that the third maximum here means the third maximum distinct number.
+# Both numbers with value 2 are both considered as second maximum.
+
+nums = [1,1,2]
+puts(third_max(nums))
+# Output: 2
+
+
+# Approach 3: Keep Track of 3 Maximums Using a Set
+# Intuition
 #
-# The maximum from our example is 12.
+# So far, our approaches have required multiple parses through the input array. While this is still O(n)O(n) in big-oh notation, it'd be good if we could solve it in a single parse. One way is to simply use a Set to keep track of the 3 maximum values we've seen so far. While you could achieve something similar using 3 variables (maximum, secondMaximum, and thirdMaximum), this is messy to work with and is poor programming practice.
 #
-# Now, we need to delete 12 from the Set. This leaves us with:
+# For each number in the Array, we add it into the Set of maximums. If this causes there to be more than 3 numbers in the Set, then we evict the smallest number.
 #
-# {3, 8, 9, 7, 4, 1}
-# We can then find and remove the second maximum, following the same process.
+# At the end, we check whether or not there are 3 numbers in the Set. If there are, this means the third maximum exists, and will be the minimum in the Set. If not, this means there was no third maximum, and so we should return the maximum of the Set, as per the problem requirements.
 #
-# The second maximum is 9 (the maximum of what's left in the Set).
-#
-# Removing it leaves us with the following:
-#
-# {3, 8, 7, 4, 1}
-# Finally, we can return the maximum of what's left, which is 8.
-#
-# Remember that if the third maximum doesn't exist, then we need to return the
-# maximum of the original Array. We can detect this situation as soon as we have
-# converted the input Array into a Set, because it will contain less than 3 values.
 
 # Complexity Analysis
-
-# Time Complexity: O(n)
-# Putting the input Array values into a HashSet has a cost of O(n), as each value
-# costs O(1) to place, and there are nn of them.
 #
-# Finding the maximum in a HashSet has a cost of O(n), as all the values need
-# to be looped through. We do this 33 times, giving O(3n) = O(n)O(3⋅n)=O(n)
-# as we drop constants in big-oh notation.
-# Deleting a value from a HashSet has a cost of O(1), so we can ignore this.
-# In total, we're left with O(n) + O(n) = O(n)+O(n)=O(n).
+# Time Complexity : O(n).
 #
-# Space Complexity: O(n)
-# In the worst case, the HashSet is the same size as the input Array,
-# and so requires O(n) space to store.
-
-
-# @param {Integer[]} nums
-# @return {Integer}
+# For each of the nn values in the input Array, we insert it into a Set for a cost of O(1).
+# We then sometimes find and remove the minimum of the Set. Because there are never more
+# than 33 items in the Set, the time complexity of doing this is O(1).
+#
+# In total, we're left with O(n).
+#
+# Space Complexity : O(1).
+#
+# Because maximums never holds more than 33 items at a time, it is considered to be constant O(1).
 require 'set'
 
 def third_max(nums)
-  nums = nums.to_set
-  maximum = nums.max
+  maximums = [].to_set
 
-  return maximum if nums.length < 3
+  nums.each do |num|
+    maximums.add(num)
+    if maximums.count > 3
+      maximums.delete(maximums.min)
+    end
+  end
 
-  nums.delete(maximum)
+  if maximums.count == 3
+    return maximums.min
+  end
 
-  second_maximum = nums.max
-  nums.delete(second_maximum)
-
-  return nums.max
+  maximums.max
 end
+
 
 nums = [3,2,1]
 print(third_max(nums))
