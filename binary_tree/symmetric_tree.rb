187b44cde9e66ebde979ceaafd8d0e6e5515bf72
Vitor Oliveira <vbrazo@gmail.com>
2021-02-13 22:40:29 -0800

    Add interative approach

--- binary_tree/symmetric_tree.rb
+++ binary_tree/symmetric_tree.rb
@@ -62,3 +62,48 @@ def is_mirror(t1, t2)
 
   return (t1.val == t2.val) && is_mirror(t1.right, t2.left) && is_mirror(t1.left, t2.right)
 end
+
+# Approach 2: Iterative
+
+# Instead of recursion, we can also use iteration with the aid of a queue.
+# Each two consecutive nodes in the queue should be equal, and their subtrees
+# a mirror of each other. Initially, the queue contains root and root.
+# Then the algorithm works similarly to BFS, with some key differences.
+# Each time, two nodes are extracted and their values compared. Then, the right
+# and left children of the two nodes are inserted in the queue in opposite order.
+# The algorithm is done when either the queue is empty, or we detect that the tree
+# is not symmetric (i.e. we pull out two consecutive nodes from the queue that are unequal).
+
+# Complexity Analysis
+
+# Time complexity: O(n). Because we traverse the entire input tree once,
+# the total run time is O(n), where nn is the total number of nodes in the tree.
+# Space complexity: There is additional space required for the search queue.
+# In the worst case, we have to insert O(n) nodes in the queue. Therefore,
+# space complexity is O(n).
+
+def is_symmetric(root)
+  return true if root.nil?
+
+  stack = []
+  stack << root.left  if root.left
+  stack << root.right if root.right
+
+  until stack.empty?
+    return false unless stack.size.even?
+
+    right = stack.pop
+    left  = stack.pop
+    return false unless right.val == left.val
+
+    return false unless left.left.nil? == right.right.nil?
+    stack << left.left   if left.left
+    stack << right.right if right.right
+
+    return false unless left.right.nil? == right.left.nil?
+    stack << left.right  if left.right
+    stack << right.left  if right.left
+  end
+
+  true
+end
