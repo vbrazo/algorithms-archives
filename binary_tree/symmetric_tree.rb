6f9d55338f87d505ac3b9b8963136fc8fd5e927e
Vitor Oliveira <vbrazo@gmail.com>
2021-02-13 22:32:17 -0800

    Update symmetric_tree.rb

--- binary_tree/symmetric_tree.rb
+++ binary_tree/symmetric_tree.rb
@@ -2,11 +2,11 @@
 
 # For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
 
-#    1
-#   / \
-#  2   2
-# / \ / \
-# 3  4 4  3
+#      1
+#    /   \
+#   2     2
+#  / \   / \
+# 3   4 4   3
 
 # But the following [1,2,2,null,3,null,3] is not:
 
