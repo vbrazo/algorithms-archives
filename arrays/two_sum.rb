be175cf697fbcca6bac228088e72dcf697d74690
Vitor Oliveira <vbrazo@gmail.com>
2021-02-14 03:20:38 -0800

    Add approach 3

--- arrays/two_sum.rb
+++ arrays/two_sum.rb
@@ -45,7 +45,68 @@ target = 6
 print(two_sum(nums, target))
 # Output: [0,1]
 
-# Approach 2: One-pass Hash Table
+
+# Approach 2: Two-pass Hash Table
+
+# To improve our run time complexity, we need a more efficient way to check
+# if the complement exists in the array. If the complement exists, we need to
+# look up its index. What is the best way to maintain a mapping of each element
+# in the array to its index? A hash table.
+#
+# We reduce the look up time from O(n) to O(1) by trading space for speed.
+# A hash table is built exactly for this purpose, it supports fast look up in
+# near constant time. I say "near" because if a collision occurred, a look up
+# could degenerate to O(n) time. But look up in hash table should be amortized
+# O(1) time as long as the hash function was chosen carefully.
+#
+# A simple implementation uses two iterations. In the first iteration, we add
+# each element's value and its index to the table. Then, in the second iteration
+# we check if each element's complement (target - nums[i]) exists
+# in the table. Beware that the complement must not be nums[i] itself!
+
+# Complexity Analysis:
+
+# Time complexity: O(n). We traverse the list containing nn elements exactly twice.
+# Since the hash table reduces the look up time to O(1), the time complexity is O(n).
+
+# Space complexity: O(n). The extra space required depends on the number of items
+# stored in the hash table, which stores exactly n elements.
+
+def two_sum(nums, target)
+  value_indices = {}
+
+  # create hash
+  for i in 0...nums.length
+    value_indices[nums[i]]= i
+  end
+
+  # iterate over nums and lookup missing target
+  for i in 0...nums.length
+    missing_value = target - nums[i]
+    if value_indices[missing_value] && value_indices[missing_value] != i
+      return [i, value_indices[missing_value]]
+    end
+  end
+end
+
+nums = [2,7,11,15]
+target = 9
+print(two_sum(nums, target))
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+nums = [3,2,4]
+target = 6
+print(two_sum(nums, target))
+# Output: [1,2]
+
+nums = [3,3]
+target = 6
+print(two_sum(nums, target))
+# Output: [0,1]
+
+
+# Approach 3: One-pass Hash Table
 
 # It turns out we can do it in one-pass. While we iterate and inserting elements
 # into the table, we also look back to check if current element's complement
@@ -55,25 +116,22 @@ print(two_sum(nums, target))
 # Complexity Analysis:
 
 # Time complexity: O(n). We traverse the list containing nn elements only once.
-# Each look up in the table costs only O(1)O(1) time.
+# Each look up in the table costs only O(1) time.
 
 # Space complexity: O(n). The extra space required depends on the number of items
 # stored in the hash table, which stores at most nn elements.
 
 def two_sum(nums, target)
-  hash = {}
-  nums.each_with_index do |n,idx|
-    case hash[n]
-      when nil then hash[n] = [idx]
-      else
-        hash[n] << idx
-      end
+  hash = Hash.new
 
-    if hash.has_key? (target-n)
-      if (!hash[target-n].include?(idx) || hash[target-n].size > 1)
-        return (hash[target-n]-[idx]).first, idx
-      end
+  for i in 0...nums.length
+    complement = target - nums[i]
+
+    if hash[complement] && i != hash[complement]
+      return [i, hash[complement]]
     end
+
+    hash[nums[i]] = i
   end
 end
 
