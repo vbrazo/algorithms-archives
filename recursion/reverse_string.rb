2e4c21c1a0cb502a99e28362ba21ef923cad2c16
Vitor Oliveira <vbrazo@gmail.com>
2021-02-14 21:25:27 -0800

    Add approach 2: two pointers

--- recursion/reverse_string.rb
+++ recursion/reverse_string.rb
@@ -46,3 +46,38 @@ print(reverse_string(["h","e","l","l","o"]))
 
 reverse_string(["H","a","n","n","a","h"])
 # Output: ["h","a","n","n","a","H"]
+
+# Approach 2: Two Pointers, Iteration, O(1) Space
+# Two Pointers Approach
+
+# In this approach, two pointers are used to process two array elements
+# at the same time. Usual implementation is to set one pointer in the beginning
+# and one at the end and then to move them until they both meet.
+# Sometimes one needs to generalize this approach in order to use three pointers,
+# like for classical Sort Colors problem.
+
+# Complexity Analysis
+
+# Time complexity: O(N) to swap N/2 element.
+# Space complexity: O(1), it's a constant space solution.
+
+# @param {Character[]} s
+# @return {Void} Do not return anything, modify s in-place instead.
+def reverse_string(s)
+  helper(s, 0, s.count - 1)
+
+  print("#{s}\n")
+end
+
+def helper(s, left, right)
+  s[left], s[right] = s[right], s[left]
+
+  left = left + 1
+  right = right - 1
+end
+
+print(reverse_string(["h","e","l","l","o"]))
+# Output: ["o","l","l","e","h"]
+
+reverse_string(["H","a","n","n","a","h"])
+# Output: ["h","a","n","n","a","H"]
