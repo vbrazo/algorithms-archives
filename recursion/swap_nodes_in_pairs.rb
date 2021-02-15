ee21adf381058c35c1f29254bd6040383e184316
Vitor Oliveira <vbrazo@gmail.com>
2021-02-15 01:11:55 -0800

    Add iterative approach

--- recursion/swap_nodes_in_pairs.rb
+++ recursion/swap_nodes_in_pairs.rb
@@ -41,3 +41,46 @@ end
 
 # Input: head = [1]
 # Output: [1]
+
+# Approach 2: Iterative Approach
+
+# Intuition
+
+# The concept here is similar to the recursive approach.
+# We break the linked list into pairs by jumping in steps of two.
+# The only difference is, unlike recursion, we swap the nodes on the go.
+# After swapping a pair of nodes, say A and B, we need to link the node B
+# to the node that was right before A. To establish this linkage we save
+# the previous node of node A in prevNode.
+
+# Complexity Analysis
+
+# Time Complexity: O(N) where N is the size of the linked list.
+# Space Complexity: O(1).
+
+# @param {ListNode} head
+# @return {ListNode}
+def swap_pairs(head)
+  dummy = ListNode.new(-1)
+  dummy.next = head
+
+  prev_node = dummy
+
+  while head and head.next
+    # Nodes to be swapped
+    first_node = head;
+    second_node = head.next;
+
+    # Swapping
+    prev_node.next = second_node
+    first_node.next = second_node.next
+    second_node.next = first_node
+
+    # Reinitializing the head and prev_node for next swap
+    prev_node = first_node
+    head = first_node.next
+  end
+
+  # Return the new head node.
+  dummy.next
+end
