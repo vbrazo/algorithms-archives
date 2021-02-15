1b9f2c448366a7dd46d89813f0f8aa69888efde0
Vitor Oliveira <vbrazo@gmail.com>
2021-02-14 20:20:39 -0800

    Add One pass algorithm

--- linked_lists/remove_nth_node_from_end_of_list.rb
+++ linked_lists/remove_nth_node_from_end_of_list.rb
@@ -82,3 +82,44 @@ def remove_nth_from_end(head, n)
 
   head
 end
+
+# Approach 2: One pass algorithm
+
+# Algorithm
+
+# The above algorithm could be optimized to one pass. Instead of one pointer,
+# we could use two pointers. The first pointer advances the list by n+1 steps
+# from the beginning, while the second pointer starts from the beginning
+# of the list. Now, both pointers are exactly separated by nn nodes apart.
+# We maintain this constant gap by advancing both pointers together until
+# the first pointer arrives past the last node. The second pointer will be
+# pointing at the nnth node counting from the last. We relink the next pointer
+# of the node referenced by the second pointer to point to the node's next next node.
+
+# Complexity Analysis
+#
+# Time complexity: O(L). The algorithm makes one traversal of the list of L nodes.
+# Space complexity: O(1).
+#
+# We only used constant extra space.
+
+# @param {ListNode} head
+# @param {Integer} n
+# @return {ListNode}
+def remove_nth_from_end(head, n)
+  root = ListNode.new(nil, head)
+
+  # left (slow) and right (fast) pointers
+  l = r = root
+
+  # gap of n nodes between l and r
+  n.times { r = r.next }
+
+  # l just before the node to remove
+  l = l.next until (r = r.next).nil?
+
+  # unlink next node
+  l.next = l.next.next
+
+  root.next
+end
