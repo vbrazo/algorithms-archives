a226417d5088fe790b100381083de0f0ca440422
Vitor Oliveira <vbrazo@gmail.com>
2021-02-18 23:09:58 -0800

    Add hash map approach

--- arrays/intersection_of_two_arrays_ii.rb
+++ arrays/intersection_of_two_arrays_ii.rb
@@ -53,3 +53,38 @@ nums1 = [4, 9, 5]
 nums2 = [9, 4, 9, 8, 4]
 intersect(nums1, nums2)
 # => [4,9]
+
+# Approach 2: Hash Map
+
+# For the previous problem, we used a hash set to achieve a linear time complexity.
+# Here, we need to use a hash map to track the count for each number.
+
+# We collect numbers and their counts from one of the arrays into a hash map.
+# Then, we iterate along the second array, and check if the number exists
+# in the hash map and its count is positive. If so - add the number to the result
+# and decrease its count in the hash map.
+
+# Complexity Analysis
+
+# Time Complexity: O(n+m), where nn and mm are the lengths of the arrays.
+# We iterate through the first, and then through the second array; insert
+# and lookup operations in the hash map take a constant time.
+
+# Space Complexity: O(min(n,m)). We use hash map to store numbers (and their
+# counts) from the smaller array.
+
+def intersect(arr1, arr2)
+  ret = []
+  hash1 = Hash.new(0)
+
+  arr2.each { |num| hash1[num] += 1 }
+
+  arr1.each do |num|
+    if hash1.has_key?(num)
+      ret << num if hash1[num] >= 1
+      hash1[num] -= 1
+    end
+  end
+
+  ret
+end
