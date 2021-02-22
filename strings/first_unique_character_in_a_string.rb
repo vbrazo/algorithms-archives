25d0f3420ed5d786de2b08a874026ebb48add1dd
Vitor Oliveira <vbrazo@gmail.com>
2021-02-21 16:40:23 -0800

    Update first_unique_character_in_a_string.rb

--- strings/first_unique_character_in_a_string.rb
+++ strings/first_unique_character_in_a_string.rb
@@ -1,4 +1,4 @@
-# Given a string, find the first non-repeating character in it =
+# Given a string, find the first non-repeating character in it
 # and return its index. If it doesn't exist, return -1.
 
 #
@@ -12,11 +12,12 @@
 # the number of times each character appears in the string.
 # That would take O(N) time, where N is a number of characters
 # in the string.
+# 
 # And then we go through the string the second time,
 # this time we use the hash map as a reference to check
-# if a character is unique or not.
-# If the character is unique, one could just return its index.
-# The complexity of the second iteration is O(N) as well.
+# if a character is unique or not. If the character is unique,
+# one could just return its index. The complexity of the second
+# iteration is O(N) as well.
 
 # Complexity Analysis
 
