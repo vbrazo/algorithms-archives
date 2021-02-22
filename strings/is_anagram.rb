899183f622f2362882684b757edf96786d0e00ff
Vitor Oliveira <vbrazo@gmail.com>
2021-02-21 16:59:24 -0800

    add more explanation

--- strings/is_anagram.rb
+++ strings/is_anagram.rb
@@ -6,6 +6,28 @@
 # Follow up:
 # What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
+
+# Approach #2 (Hash Table)
+
+# Algorithm
+#
+# To examine if tt is a rearrangement of ss, we can count
+# occurrences of each letter in the two strings and compare
+# them. Since both ss and tt contain only letters from a-z,
+# a simple counter table of size 26 is suffice.
+# Do we need two counter tables for comparison? Actually no,
+# because we could increment the counter for each letter in s
+# and decrement the counter for each letter in t, then check
+# if the counter reaches back to zero.
+
+# Complexity analysis
+#
+# Time complexity : O(n). Time complexity is O(n) because
+# accessing the counter table is a constant time operation.
+# Space complexity : O(1). Although we do use extra space,
+# the space complexity is O(1) because the table's size stays
+# constant no matter how large n is.
+
 # @param {String} s
 # @param {String} t
 # @return {Boolean}
