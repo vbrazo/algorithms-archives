9d73f88a7c62681205c0c7f01666cef7082cee2c
Vitor Oliveira <vbrazo@gmail.com>
2021-02-25 00:04:35 -0800

    Approach 4: Using Reverse

--- arrays/rotate_array.rb
+++ arrays/rotate_array.rb
@@ -167,3 +167,68 @@ rotate(nums, k)
 # Explanation:
 # rotate 1 steps to the right: [99,-1,-100,3]
 # rotate 2 steps to the right: [3,99,-1,-100]
+
+#
+# Approach 4: Using Reverse
+#
+
+# Algorithm
+#
+# This approach is based on the fact that when we rotate
+# the array k times, k%nk elements from the back end of
+# the array come to the front and the rest of the elements
+# from the front shift backwards.
+#
+# In this approach, we firstly reverse all the elements
+# of the array. Then, reversing the first k elements
+# followed by reversing the rest n-k elements gives us
+# the required result.
+#
+# Let n = 7 and k = 3.
+#
+# Original List                   : 1 2 3 4 5 6 7
+# After reversing all numbers     : 7 6 5 4 3 2 1
+# After reversing first k numbers : 5 6 7 4 3 2 1
+# After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
+
+# Complexity Analysis
+
+# Time complexity: O(n). nn elements are reversed a total of three times.
+# Space complexity: O(1). No extra space is used.
+
+def reverse(nums, start, _end)
+  while start < _end
+    nums[start], nums[_end] = nums[_end], nums[start]
+    start += 1
+    _end -= 1
+  end
+end
+
+def rotate(nums, k)
+  n = nums.count
+
+  reverse(nums, 0, n - 1)
+  reverse(nums, 0, k - 1)
+  reverse(nums, k, n - 1)
+
+  print nums
+end
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+rotate(nums, k)
+# => [5,6,7,1,2,3,4]
+
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+nums = [-1, -100, 3, 99]
+k = 2
+rotate(nums, k)
+# => [3,99,-1,-100]
+
+# Explanation:
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
