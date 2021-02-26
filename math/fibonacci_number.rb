4eea6a72c7952f62a5bd774a789ed9b4e20df67a
Vitor Oliveira <vbrazo@gmail.com>
2021-02-25 22:33:10 -0800

    Top-Down Approach using Memoization

--- math/fibonacci_number.rb
+++ math/fibonacci_number.rb
@@ -6,9 +6,9 @@
 # F(n) = F(n - 1) + F(n - 2), for n > 1.
 # Given n, calculate F(n).
 
-# 
+#
 # Approach 1
-# 
+#
 
 # Complexity Analysis
 #
@@ -49,9 +49,9 @@ n = 4
 # Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 puts(fib(n))
 
-# 
+#
 # Approach 2: Bottom-Up Approach using Memoization
-# 
+#
 
 # Intuition
 
@@ -88,6 +88,64 @@ def memoize(n)
   end
 
   cache[n]
+end
+
+n = 2
+# Output: 1
+# Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+puts(fib(n))
+
+n = 3
+# Output: 2
+# Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+puts(fib(n))
+
+n = 4
+# Output: 3
+# Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+puts(fib(n))
+
+# Approach 3: Top-Down Approach using Memoization
+
+# Intuition
+#
+# Solve for all of the sub-problems, use memoization to store the
+# pre-computed answers, then return the answer for N. We will leverage
+# recursion, but in a smarter way by not repeating the work to
+# calculate existing values.
+#
+# Algorithm
+#
+# Check if N <= 1. If it is, return N.
+# Call and return memoize(N)
+# If N exists in the map, return the cached value for N
+# Otherwise set the value of N, in our mapping, to the value of
+# memoize(N-1) + memoize(N-2)
+
+# Complexity Analysis
+
+# Time complexity: O(N). Each number, starting at 2 up to and
+# including N, is visited, computed and then stored for O(1) access
+# later on.
+# Space complexity : O(N). The size of the stack in memory is
+# proportionate to N.
+
+def fib(n)
+  return n if n <= 1
+
+  cache = {}
+  cache[0] = 0
+  cache[1] = 1
+
+  memoize(n, cache)
+end
+
+def memoize(n, cache)
+  return cache[n] if cache.keys.include? n
+
+  cache[n] = memoize(n - 1, cache) + memoize(n - 2, cache)
+
+  memoize(n, cache)
 end
 
 n = 2
