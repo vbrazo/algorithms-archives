bc4cdfb3d44958c3c2075d9f9095324bdaed5040
Vitor Oliveira <vbrazo@gmail.com>
2021-02-26 22:15:01 -0800

    Solve it with recursion with memoization

--- arrays/climbing_stairs.rb
+++ arrays/climbing_stairs.rb
@@ -53,3 +53,55 @@ print(climb_stairs(n))
 # 1. 1 step + 1 step + 1 step
 # 2. 1 step + 2 steps
 # 3. 2 steps + 1 step
+
+#
+# Approach 2: Recursion with Memoization
+#
+
+# Algorithm
+
+# In the previous approach we are redundantly calculating the result for every
+# step. Instead, we can store the result at each step in memomemo array and
+# directly returning the result from the memo array whenever that function is
+# called again.
+#
+# In this way we are pruning recursion tree with the help of memomemo array
+# and reducing the size of recursion tree upto nn.
+
+# Complexity Analysis
+
+# Time complexity: O(n). Size of recursion tree can go upto n.
+# Space complexity: O(n). The depth of recursion tree can go upto n.
+
+# @param {Integer} n
+# @return {Integer}
+def climb_stairs(n)
+  # consider an array of [1,2]
+  # with each recursion, you can only really chose either or
+  # and if the total == your argument, then that is 1 step found
+
+  def dp(n, memo)
+    return 0 if n < 0
+    return 1 if n == 0
+    return memo[n] if memo.has_key?(n)
+
+    memo[n] = dp(n - 1, memo) + dp(n - 2, memo)
+  end
+
+  dp(n, {})
+end
+
+n = 2
+print(climb_stairs(n))
+# => 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+n = 3
+print(climb_stairs(n))
+# => 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
