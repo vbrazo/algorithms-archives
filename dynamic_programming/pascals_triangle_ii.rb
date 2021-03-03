5328c231c090cbafc89ad92eabcab56315f9bc62
Vitor Oliveira <vbrazo@gmail.com>
2021-03-02 20:23:01 -0800

    Add math approach

--- dynamic_programming/pascals_triangle_ii.rb
+++ dynamic_programming/pascals_triangle_ii.rb
@@ -165,3 +165,40 @@ print(get_row(row_index))
 row_index = 1
 print(get_row(row_index))
 # => [1,1]
+
+#
+# Approach 4: Math! (specifically, Combinatorics)
+#
+
+# Complexity Analysis
+
+# Time complexity: O(k)O.
+# Each term is calculated once, in constant time.
+
+# Space complexity: O(k)O.
+# No extra space required other than that required to
+# hold the output.
+
+def get_row(row_index)
+  (0..row_index).map { |num| combination(row_index, num) }
+end
+
+def combination(num1, num2)
+  factorial(num1) / (factorial(num2) * factorial(num1 - num2))
+end
+
+def factorial(num)
+  (1..num).inject(1) { |res, i| res * i }
+end
+
+row_index = 3
+print(get_row(row_index))
+# => [1,3,3,1]
+
+row_index = 0
+print(get_row(row_index))
+# => [1]
+
+row_index = 1
+print(get_row(row_index))
+# => [1,1]
