17f62590018a6a6e6c1dddd43bb532189cb59a3b
Vitor Oliveira <vbrazo@gmail.com>
2021-03-06 13:04:37 -0800

    Hash table approach

---
+++ arrays/number_of_good_pairs.rb
@@ -0,0 +1,63 @@
+# Given an array of integers nums.
+
+# A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+# Return the number of good pairs.
+
+# Example 1:
+#
+# Input: nums = [1,2,3,1,1,3]
+# Output: 4
+# Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+#
+# Example 2:
+#
+# Input: nums = [1,1,1,1]
+# Output: 6
+# Explanation: Each pair in the array are good.
+#
+# Example 3:
+#
+# Input: nums = [1,2,3]
+# Output: 0
+
+#
+# Approach 1: Hash table
+#
+
+# @param {Integer[]} nums
+# @return {Integer}
+def num_identical_pairs(nums)
+  hash = {}
+
+  nums.each do |num|
+    if hash[num]
+      hash[num] = hash[num] + 1
+    else
+      hash[num] = 1
+    end
+  end
+
+  counter = 0
+
+  # Count how many times each number appears.
+  # If a number appears n times, then n * (n – 1) // 2 good pairs
+  # can be made with this number.
+  hash.each do |key, val|
+    counter = counter + (val * (val - 1) / 2)
+  end
+
+  counter
+end
+
+nums = [1,2,3,1,1,3]
+puts(num_identical_pairs(nums))
+# Output: 4
+
+nums = [1,1,1,1]
+puts(num_identical_pairs(nums))
+# Output: 6
+
+nums = [1,2,3]
+puts(num_identical_pairs(nums))
+# Output: 0
