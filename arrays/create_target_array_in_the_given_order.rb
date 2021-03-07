8888deb1c80c514e6d66daa8dc0e6bf88c1313a6
Vitor Oliveira <vbrazo@gmail.com>
2021-03-06 21:23:55 -0800

    Add approach 2

--- arrays/create_target_array_in_the_given_order.rb
+++ arrays/create_target_array_in_the_given_order.rb
@@ -67,3 +67,48 @@ nums = [1]
 index = [0]
 create_target_array(nums, index)
 # Output: [1]
+
+#
+# Approach 2:
+#
+# Append every new element to the end of the
+# resulting array, and make right part of the array move
+# one step forward by swapping new element with left neighbor.
+# Swap until new element's index is equal to the required index
+# from index array.
+#
+
+# @param {Integer[]} nums
+# @param {Integer[]} index
+# @return {Integer[]}
+def create_target_array(nums, index)
+  result = []
+
+  nums.each_with_index do |value, indx|
+    i = index[indx]
+    result.push(value)
+    j = result.size - 1
+
+    while i != j
+      result[j - 1], result[j] = result[j], result[j - 1]
+      j -= 1
+    end
+  end
+
+  result
+end
+
+nums = [0, 1, 2, 3, 4]
+index = [0, 1, 2, 2, 1]
+create_target_array(nums, index)
+# Output: [0,4,1,3,2]
+
+nums = [1, 2, 3, 4, 0]
+index = [0, 1, 2, 3, 0]
+create_target_array(nums, index)
+# Output: [0,1,2,3,4]
+
+nums = [1]
+index = [0]
+create_target_array(nums, index)
+# Output: [1]
