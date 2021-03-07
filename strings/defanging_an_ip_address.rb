49e5e7a5e094c260d2030c741b96a0f17790e300
Vitor Oliveira <vbrazo@gmail.com>
2021-03-07 15:55:05 -0800

    Add defanging an ip address description

---
+++ strings/defanging_an_ip_address.rb
@@ -0,0 +1,25 @@
+# Given a valid (IPv4) IP address, return a defanged version of
+# that IP address. A defanged IP address replaces every period
+# "." with "[.]".
+
+# Example 1:
+#
+# Input: address = "1.1.1.1"
+# Output: "1[.]1[.]1[.]1"
+
+# Example 2:
+#
+# Input: address = "255.100.50.0"
+# Output: "255[.]100[.]50[.]0"
+
+# @param {String} address
+# @return {String}
+def defang_i_paddr(address); end
+
+address = '1.1.1.1'
+print(defang_i_paddr(address))
+# Output: "1[.]1[.]1[.]1"
+
+address = '1.1.1.1'
+print(defang_i_paddr(address))
+# Output: "255[.]100[.]50[.]0"
