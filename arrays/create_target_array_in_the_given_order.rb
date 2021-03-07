e93df3ab4a3208b2b07ff05ad504caf9c4cd33f7
Vitor Oliveira <vbrazo@gmail.com>
2021-03-06 21:23:48 -0800

    Add approach 1

---
+++ arrays/create_target_array_in_the_given_order.rb
@@ -0,0 +1,69 @@
+# Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+#
+# Initially target array is empty.
+# From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+# Repeat the previous step until there are no elements to read in nums and index.
+# Return the target array.
+#
+# It is guaranteed that the insertion operations will be valid.
+
+# Example 1:
+#
+# Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+# Output: [0,4,1,3,2]
+# Explanation:
+# nums       index     target
+# 0            0        [0]
+# 1            1        [0,1]
+# 2            2        [0,1,2]
+# 3            2        [0,1,3,2]
+# 4            1        [0,4,1,3,2]
+
+# Example 2:
+#
+# Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+# Output: [0,1,2,3,4]
+# Explanation:
+# nums       index     target
+# 1            0        [1]
+# 2            1        [1,2]
+# 3            2        [1,2,3]
+# 4            3        [1,2,3,4]
+# 0            0        [0,1,2,3,4]
+
+# Example 3:
+#
+# Input: nums = [1], index = [0]
+# Output: [1]
+
+#
+# Approach 1
+#
+
+# @param {Integer[]} nums
+# @param {Integer[]} index
+# @return {Integer[]}
+def create_target_array(nums, index)
+  arr = []
+
+  nums.count.times do |i|
+    arr.insert(index[i], nums[i])
+  end
+
+  arr
+end
+
+nums = [0, 1, 2, 3, 4]
+index = [0, 1, 2, 2, 1]
+create_target_array(nums, index)
+# Output: [0,4,1,3,2]
+
+nums = [1, 2, 3, 4, 0]
+index = [0, 1, 2, 3, 0]
+create_target_array(nums, index)
+# Output: [0,1,2,3,4]
+
+nums = [1]
+index = [0]
+create_target_array(nums, index)
+# Output: [1]
