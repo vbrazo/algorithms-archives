a82f9ddf34cf3701de9d2b09c0b5f302264448d5
Vitor Oliveira <vbrazo@gmail.com>
2021-03-07 09:34:58 -0800

    Remove vowels from a string

---
+++ strings/remove_vowels_from_a_string.rb
@@ -0,0 +1,33 @@
+# Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u'
+# from it, and return the new string.
+
+# Example 1:
+#
+# Input: s = "leetcodeisacommunityforcoders"
+# Output: "ltcdscmmntyfrcdrs"
+
+# Example 2:
+#
+# Input: s = "aeiou"
+# Output: ""
+
+# @param {String} s
+# @return {String}
+def remove_vowels(s)
+  new_str = []
+  vowels = ['a', 'e', 'i', 'o', 'u']
+
+  s.to_s.chars.each do |character|
+    new_str.push(character) unless vowels.include? character
+  end
+
+  new_str.join
+end
+
+s = "leetcodeisacommunityforcoders"
+print(remove_vowels(s))
+# Output: "ltcdscmmntyfrcdrs"
+
+s = "aeiou"
+print(remove_vowels(s))
+# Output: ""
