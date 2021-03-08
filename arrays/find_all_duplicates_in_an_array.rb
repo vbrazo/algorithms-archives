ffd66ef7e0c3dc710e575ef07492c903d1a6fabb
Vitor Oliveira <vbrazo@gmail.com>
2021-03-07 17:24:32 -0800

    Hash map approach

--- arrays/find_all_duplicates_in_an_array.rb
+++ arrays/find_all_duplicates_in_an_array.rb
@@ -14,12 +14,33 @@
 # Output:
 # [2,3]
 
+#
+# Approach 1: Hash map
+#
+
+# Complexity Analysis
+#
+# Time complexity: O(n) average case.
+# Space complexity: Upto O(n) extra space required for the set.
+#
+
 # @param {Integer[]} nums
 # @return {Integer[]}
 def find_duplicates(nums)
+  hash = {}
 
+  nums.each do |number|
+    if hash[number].nil?
+      hash[number] = 1
+    else
+      hash[number] = hash[number] + 1
+    end
+  end
+
+  hash = hash.select { |key, value| value > 1 }
+  hash.keys
 end
 
 nums = [4,3,2,7,8,2,3,1]
-find_duplicates(nums)
+print(find_duplicates(nums))
 # Output: [2,3]
