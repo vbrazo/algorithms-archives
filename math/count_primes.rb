1600970c82578d38561a0659d93254a23b79c76f
Vitor Oliveira <vbrazo@gmail.com>
2021-03-07 16:14:05 -0800

    Add more explanation

--- math/count_primes.rb
+++ math/count_primes.rb
@@ -32,14 +32,16 @@
 # if n is divisible by some number p, then n = p × q and since p ≤ q,
 # we could derive that p ≤ √n.
 #
-# Our total runtime has now improved to O(n1.5), which is slightly
+# Our total runtime has now improved to O(n^1.5), which is slightly
 # better. Is there a faster approach?
 
 def count_primes(n)
   count = 0
+
   n.times do |i|
     count += 1 if is_prime(i)
   end
+
   count
 end
 
@@ -49,6 +51,7 @@ def is_prime(num)
   # Loop's ending condition is i * i <= num instead of i <= sqrt(num)
   # to avoid repeatedly calling an expensive function sqrt().
   i = 2
+
   while i * i <= num
     return false if num % i == 0
 
@@ -87,6 +90,31 @@ puts(count_primes(4))
 # was already marked off. What does this tell you? Should you mark off
 # all multiples of 4 as well?
 
+# 4 is not a prime because it is divisible by 2, which means all
+# multiples of 4 must also be divisible by 2 and were already marked
+# off. So we can skip 4 immediately and go to the next number, 5.
+# Now, all multiples of 5 such as
+# 5 × 2 = 10, 5 × 3 = 15, 5 × 4 = 20, 5 × 5 = 25, ...
+# can be marked off. There is a slight optimization here, we do not
+# need to start from 5 × 2 = 10. Where should we start marking off?
+
+# In fact, we can mark off multiples of 5 starting at 5 × 5 = 25,
+# because 5 × 2 = 10 was already marked off by multiple of 2, similarly
+# 5 × 3 = 15 was already marked off by multiple of 3. Therefore, if
+# the current number is p, we can always mark off multiples of p
+# starting at p2, then in increments of p: p2 + p, p2 + 2p, ... Now
+# what should be the terminating loop condition?
+
+# It is easy to say that the terminating loop condition is p < n,
+# which is certainly correct but not efficient.
+
+# Yes, the terminating loop condition can be p < √n, as all
+# non-primes ≥ √n must have already been marked off. When the loop
+# terminates, all the numbers in the table that are non-marked are prime.
+
+# The Sieve of Eratosthenes uses an extra O(n) memory and its runtime
+# complexity is O(n log log n).
+
 def count_primes(n)
   # assume everything is prime
   sieve = [true] * n
@@ -102,7 +130,8 @@ def count_primes(n)
       while i * j < n
         # mark it as not prime
         sieve[i * j] = false
-         # so we can skip it next time
+
+        # so we can skip it next time
         j += 1
       end
     end
@@ -131,8 +160,9 @@ puts(count_primes(4))
 #
 
 require 'prime'
+
 def count_primes(n)
-   Prime.each(n - 1).count
+  Prime.each(n - 1).count
 end
 
 puts(count_primes(10))
