52d95e3f0ce48743643b1cbff001d7e06e2e32e8
Vitor Oliveira <vbrazo@gmail.com>
2021-03-07 16:52:21 -0800

    Add description

---
+++ arrays/find_all_duplicates_in_an_array.rb
@@ -0,0 +1,25 @@
+# Find All Duplicates in an Array
+#
+# Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array),
+# some elements appear twice and others appear once.
+#
+# Find all the elements that appear twice in this array.
+#
+# Could you do it without extra space and in O(n) runtime?
+#
+# Example:
+# Input:
+# [4,3,2,7,8,2,3,1]
+#
+# Output:
+# [2,3]
+
+# @param {Integer[]} nums
+# @return {Integer[]}
+def find_duplicates(nums)
+
+end
+
+nums = [4,3,2,7,8,2,3,1]
+find_duplicates(nums)
+# Output: [2,3]
