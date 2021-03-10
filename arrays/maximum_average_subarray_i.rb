a3d3139c2f4a2ee17bc285159c2fa7fe8bc87f6e
Vitor Oliveira <vbrazo@gmail.com>
2021-03-09 20:22:14 -0800

    Add find max average

---
+++ arrays/maximum_average_subarray_i.rb
@@ -0,0 +1,22 @@
+# Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value. And you need to output the maximum average value.
+#
+# Example 1:
+#
+# Input: nums = [1,12,-5,-6,50,3], k = 4
+# Output: 12.75
+# Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Float}
+def find_max_average(nums, k)
+  max = 0
+  pointer1 = 0
+
+  sum / k
+end
+
+nums = [1,12,-5,-6,50,3]
+k = 4
+puts find_max_average(nums, k)
+# Output: 12.75
