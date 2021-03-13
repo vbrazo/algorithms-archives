f4dbd1e1e512689b007eab89f54fb6fb858ddc32
Vitor Oliveira <vbrazo@gmail.com>
2021-03-13 14:12:26 -0800

    add hash map approach

--- arrays/dot_product_of_two_sparse_vectors.rb
+++ arrays/dot_product_of_two_sparse_vectors.rb
@@ -10,7 +10,11 @@
 #
 # Follow up: What if only one of the vectors is sparse?
 
+#
+# Approach 1: Non-efficient Array Approach
+#
 
+#
 # Complexity Analysis
 #
 # Let n be the length of the input array.
@@ -39,6 +43,84 @@ class SparseVector
     end
 
     sum
+  end
+end
+
+nums1 = [1, 0, 0, 2, 3]
+nums2 = [0, 3, 0, 4, 0]
+v1 = SparseVector.new(nums1)
+v2 = SparseVector.new(nums2)
+ans = v1.dotProduct(v2)
+puts(ans)
+# Output: 8
+
+nums1 = [0, 1, 0, 0, 0]
+nums2 = [0, 0, 0, 0, 2]
+v1 = SparseVector.new(nums1)
+v2 = SparseVector.new(nums2)
+ans = v1.dotProduct(v2)
+puts(ans)
+# Output: 0
+
+nums1 = [0, 1, 0, 0, 2, 0, 0]
+nums2 = [1, 0, 0, 0, 3, 0, 4]
+v1 = SparseVector.new(nums1)
+v2 = SparseVector.new(nums2)
+ans = v1.dotProduct(v2)
+puts(ans)
+# Output: 6
+
+nums1 = [0, 0, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0]
+nums2 = [0, 0, 2, 0, 0, 4, 3, 0, 0, 2, 0, 0, 0]
+v1 = SparseVector.new(nums1)
+v2 = SparseVector.new(nums2)
+ans = v1.dotProduct(v2)
+puts(ans)
+# Output: 15
+
+#
+# Approach 2: Hash set
+#
+
+# Intuition
+
+# Store the non-zero values and their corresponding indices in a dictionary,
+# with the index being the key. Any index that is not present corresponds to a
+# value 0 in the input array.
+
+# Complexity Analysis
+
+# Let n be the length of the input array and L be the number of non-zero elements.
+
+# Time complexity: O(n) for creating the Hash Map;
+# O(L) for calculating the dot product.
+
+# Space complexity: O(L) for creating the Hash Map, as we only store
+# elements that are non-zero. O(1) for calculating the dot product.
+
+# iterate through each non-zero element in this sparse vector
+# update the dot product if the corresponding index has a
+# non-zero value in the other vector
+
+class SparseVector
+  def initialize(nums)
+    @non_zeros = {}
+
+    nums.each_with_index do |num, index|
+      non_zeros[index] = num
+    end
+  end
+
+  attr_reader :non_zeros
+
+  def dotProduct(vec)
+    result = 0
+
+    non_zeros.each do |key, value|
+      result += value * vec.non_zeros[key]
+    end
+
+    result
   end
 end
 
