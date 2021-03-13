2e53778db513eb7dc7de2cdaca665bf380b36591
Vitor Oliveira <vbrazo@gmail.com>
2021-03-13 14:12:38 -0800

    add two-pointers approach

--- arrays/dot_product_of_two_sparse_vectors.rb
+++ arrays/dot_product_of_two_sparse_vectors.rb
@@ -155,3 +155,90 @@ v2 = SparseVector.new(nums2)
 ans = v1.dotProduct(v2)
 puts(ans)
 # Output: 15
+
+#
+# Approach 3: Index-Value Pairs
+#
+
+# Intuition
+#
+# We can also represent elements of a sparse vector as a list of <index, value>
+# pairs. We use two pointers to iterate through the two vectors to calculate the
+# dot product.
+
+# Complexity Analysis
+#
+# Let n be the length of the input array and L and L2 be the number of non-zero
+# elements for the two vectors.
+#
+# Time complexity: O(n) for creating the <index, value> pair for non-zero values;
+# O(L+L2) for calculating the dot product.
+#
+# Space complexity: O(L) for creating the <index, value> pairs for non-zero values.
+# O(1) for calculating the dot product.
+#
+
+class SparseVector
+  def initialize(nums)
+    pairs = []
+
+    nums.each_with_index do |value, index|
+      pairs.push([index, value]) if value != 0
+    end
+
+    @pairs = pairs
+  end
+
+  attr_reader :pairs
+
+  def dotProduct(vec)
+    p = q = result = 0
+
+    while p < pairs.count && q < vec.pairs.count
+      if pairs[p][0] == vec.pairs[q][0]
+        result += pairs[p][1] * vec.pairs[q][1]
+
+        p += 1
+        q += 1
+      elsif pairs[p][0] < vec.pairs[q][0]
+        p += 1
+      else
+        q += 1
+      end
+    end
+
+    result
+  end
+end
+
+nums1 = [1, 0, 0, 2, 3]
+nums2 = [0, 3, 0, 4, 0]
+v1 = SparseVector.new(nums1)
+v2 = SparseVector.new(nums2)
+ans = v1.dotProduct(v2)
+puts(ans)
+# Output: 8
+
+nums1 = [0, 1, 0, 0, 0]
+nums2 = [0, 0, 0, 0, 2]
+v1 = SparseVector.new(nums1)
+v2 = SparseVector.new(nums2)
+ans = v1.dotProduct(v2)
+puts(ans)
+# Output: 0
+
+nums1 = [0, 1, 0, 0, 2, 0, 0]
+nums2 = [1, 0, 0, 0, 3, 0, 4]
+v1 = SparseVector.new(nums1)
+v2 = SparseVector.new(nums2)
+ans = v1.dotProduct(v2)
+puts(ans)
+# Output: 6
+
+nums1 = [0, 0, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0]
+nums2 = [0, 0, 2, 0, 0, 4, 3, 0, 0, 2, 0, 0, 0]
+v1 = SparseVector.new(nums1)
+v2 = SparseVector.new(nums2)
+ans = v1.dotProduct(v2)
+puts(ans)
+# Output: 15
