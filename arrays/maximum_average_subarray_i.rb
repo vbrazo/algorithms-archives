be3229141d9f7d6c8fb13ffecca0a663d9398746
Vitor Oliveira <vbrazo@gmail.com>
2021-03-13 10:10:05 -0800

    Add sliding window approach

--- arrays/maximum_average_subarray_i.rb
+++ arrays/maximum_average_subarray_i.rb
@@ -6,17 +6,40 @@
 # Output: 12.75
 # Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
 
+#
+# Approach: Sliding Window
+#
+
+#
+# Complexity Analysis
+#
+# Time complexity: O(n). We iterate over the given nums
+# array of length n once only.
+#
+# Space complexity: O(1). Constant extra space is used.
+#
+
 # @param {Integer[]} nums
 # @param {Integer} k
 # @return {Float}
 def find_max_average(nums, k)
-  max = 0
   pointer1 = 0
+  pointer2 = k
+  max_average = 0
 
-  sum / k
+  (k..nums.count).each do
+    new_average = nums[pointer1..pointer2-1].sum / (k * 1.0)
+
+    max_average = new_average if max_average < new_average
+
+    pointer1 += 1
+    pointer2 += 1
+  end
+
+  max_average
 end
 
-nums = [1,12,-5,-6,50,3]
+nums = [1, 12, -5, -6, 50, 3]
 k = 4
 puts find_max_average(nums, k)
 # Output: 12.75
