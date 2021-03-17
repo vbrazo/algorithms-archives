43734c255455b76e394f7e7cf40e2187c844878c
Vitor Oliveira <vbrazo@gmail.com>
2021-03-16 23:49:52 -0700

    add calls

--- backtracking/combination_sum.rb
+++ backtracking/combination_sum.rb
@@ -1,4 +1,3 @@
-
 # Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
 #
 # The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
@@ -28,7 +27,7 @@
 #
 # Input: candidates = [1], target = 1
 # Output: [[1]]
-# 
+#
 # Example 5:
 #
 # Input: candidates = [1], target = 2
@@ -75,3 +74,28 @@ def find_combinations(results, curr, candidates, target, idx=0)
     end
   end
 end
+
+candidates = [2,3,6,7]
+target = 7
+combination_sum(candidates, target)
+# Output: [[2,2,3],[7]]
+
+candidates = [2,3,5]
+target = 8
+combination_sum(candidates, target)
+# Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+candidates = [2]
+target = 1
+combination_sum(candidates, target)
+# Output: []
+
+candidates = [1]
+target = 1
+combination_sum(candidates, target)
+# Output: [[1]]
+
+candidates = [1]
+target = 2
+combination_sum(candidates, target)
+# Output: [[1,1]]
