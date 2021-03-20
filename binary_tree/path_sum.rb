e8086145ccb9edf7e5a0233dcea0d694b4ecb049
Vitor Oliveira <vbrazo@gmail.com>
2021-03-20 10:55:12 -0700

    add explanation and time complexity

--- binary_tree/path_sum.rb
+++ binary_tree/path_sum.rb
@@ -14,15 +14,34 @@
 # Input: root = [1,2], targetSum = 0
 # Output: false
 
-# Definition for a binary tree node.
-# class TreeNode
-#     attr_accessor :val, :left, :right
-#     def initialize(val = 0, left = nil, right = nil)
-#         @val = val
-#         @left = left
-#         @right = right
-#     end
-# end
+#
+# Approach 1: Recursion
+#
+#
+# The most intuitive way is to use a recursion here. One is going through the tree by considering at each step the node itself and its children. If node is not a leaf, one calls recursively hasPathSum method for its children with a sum decreased by the current node value. If node is a leaf, one checks if the the current sum is zero, i.e if the initial sum was discovered.
+#
+
+# Complexity Analysis
+#
+# Time complexity: we visit each node exactly once, thus the time complexity
+# is O(N), where N is the number of nodes.
+#
+# Space complexity: in the worst case, the tree is completely unbalanced,
+# e.g. each node has only one child node, the recursion call would occur N times
+# (the height of the tree), therefore the storage to keep the call stack
+# would be O(N). But in the best case (the tree is completely balanced),
+# the height of the tree would be log(N). Therefore, the space complexity
+# in this case would be O(log(N)).
+
+class TreeNode
+  attr_accessor :val, :left, :right
+
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
 # @param {TreeNode} root
 # @param {Integer} target_sum
 # @return {Boolean}
