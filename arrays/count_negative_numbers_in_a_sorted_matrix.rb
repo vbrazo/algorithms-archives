dfdd09d7fa11b8d75b69dfc4e39d337befbc3abe
Vitor Oliveira <vbrazo@gmail.com>
2021-04-02 15:39:23 -0700

    add Binary search approach

--- arrays/count_negative_numbers_in_a_sorted_matrix.rb
+++ arrays/count_negative_numbers_in_a_sorted_matrix.rb
@@ -54,3 +54,60 @@ puts count_negatives(grid)
 grid = [[-1]]
 puts count_negatives(grid)
 # Output: 1
+
+#
+# Approach 2: Binary search
+#
+
+# @param {Integer[][]} grid
+# @return {Integer}
+def count_negatives(grid)
+  cols = grid[0].length
+  rows = grid.length
+  count = 0
+
+  rows.each do |i|
+    if grid[i][0] < 0
+      count += cols
+      next
+    end
+
+    next if grid[i][cols - 1] > 0
+
+    left = 0
+    right = cols - 1
+
+    while left <= right
+      mid = (left + right) / 2
+
+      if grid[i][mid] < 0
+        if mid == 0 || grid[i][mid - 1] >= 0
+          count += (cols - mid)
+          break
+        end
+
+        right = mid - 1
+      else
+        left = mid + 1
+      end
+    end
+  end
+
+  count
+end
+
+grid = [[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]]
+puts count_negatives(grid)
+# Output: 8
+
+grid = [[3, 2], [1, 0]]
+puts count_negatives(grid)
+# Output: 0
+
+grid = [[1, -1], [-1, -1]]
+puts count_negatives(grid)
+# Output: 3
+
+grid = [[-1]]
+puts count_negatives(grid)
+# Output: 1
