1a3d646a552b9e8ead46cccec6fe33fdf719875d
Vitor Oliveira <vbrazo@gmail.com>
2021-04-02 11:06:15 -0700

    add minor test examples

--- hash_table/largest_unique_number.rb
+++ hash_table/largest_unique_number.rb
@@ -22,7 +22,7 @@
 
 #
 # Approach: Hash table
-# 
+#
 
 # @param {Integer[]} a
 # @return {Integer}
@@ -41,3 +41,11 @@ def largest_unique_number(a)
 
   max
 end
+
+a = [5,7,3,9,4,9,8,3,1]
+puts largest_unique_number(a)
+# Output: 8
+
+a = [9,9,8,8]
+puts largest_unique_number(a)
+# Output: -1
