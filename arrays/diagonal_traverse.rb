1e2db67b1783381961b5d32d0ea680838831f855
Vitor Oliveira <vbrazo@gmail.com>
2021-04-09 22:24:01 -0700

    diagonal traverse description

--- arrays/diagonal_traverse.rb
+++ arrays/diagonal_traverse.rb
@@ -1,3 +1,14 @@
+# Given a matrix of M x N elements (M rows, N columns), return all elements of the matrix in diagonal order.
+# 
+# Input:
+# [
+#  [ 1, 2, 3 ],
+#  [ 4, 5, 6 ],
+#  [ 7, 8, 9 ]
+# ]
+# 
+# Output: [1,2,4,7,5,3,6,8,9]
+
 # @param {Integer[][]} matrix
 # @return {Integer[]}
 def find_diagonal_order(matrix)
