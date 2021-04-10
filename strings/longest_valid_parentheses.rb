514421827fb16d4d36a5b728e24af36617469dc0
Vitor Oliveira <vbrazo@gmail.com>
2021-04-09 22:28:28 -0700

    Update longest_valid_parentheses.rb

--- strings/longest_valid_parentheses.rb
+++ strings/longest_valid_parentheses.rb
@@ -1,3 +1,19 @@
+# Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+# 
+# Example 1:
+# Input: s = "(()"
+# Output: 2
+# Explanation: The longest valid parentheses substring is "()".
+# 
+# Example 2:
+# Input: s = ")()())"
+# Output: 4
+# Explanation: The longest valid parentheses substring is "()()".
+# 
+# Example 3:
+# Input: s = ""
+# Output: 0
+
 # O(N) time
 
 def longest_valid_parentheses(s)
