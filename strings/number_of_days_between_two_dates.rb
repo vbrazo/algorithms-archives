2fdb0f0a751d72da70e3ee2a05492b90d7fefa96
Vitor Oliveira <vbrazo@gmail.com>
2021-04-30 18:33:48 -0700

    add algorithm description

--- strings/number_of_days_between_two_dates.rb
+++ strings/number_of_days_between_two_dates.rb
@@ -1,3 +1,6 @@
+# Write a program to count the number of days between two dates.
+# The two dates are given as strings, their format is YYYY-MM-DD as shown in the examples.
+
 require "date"
 
 def days_between_dates(date1, date2)
