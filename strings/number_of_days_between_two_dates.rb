4263278a95533947f853f090811bf3b7ed032fad
Vitor Oliveira <vbrazo@gmail.com>
2021-04-30 18:31:15 -0700

    use the Date library

--- strings/number_of_days_between_two_dates.rb
+++ strings/number_of_days_between_two_dates.rb
@@ -1,29 +1,17 @@
-def day(y, m, d)
-  yd = 365 * (y-1970)
-  for i in range(1970, y)
-    if i % 4 == 0
-      yd += 1
-    end
-  end
+require "date"
 
-  month = [31,28,31,30,31,30,31,31,30,31,30,31]
-  md = month.sum
-  if y % 4 == 0 and y != 2100 and m > 2
-    md += 1
-  end
-
-  return yd + md + d
+def days_between_dates(date1, date2)
+  date1 = Date.parse(date1).to_datetime.strftime("%s").to_i
+  date2 = Date.parse(date2).to_datetime.strftime("%s").to_i
+  return (date2 - date1).abs / (60*60*24)
 end
 
-y1 = int(date1[:4])
-m1 = int(date1[5:7])
-d1 = int(date1[8:])
+date1 = "2019-06-29"
+date2 = "2019-06-30"
+puts days_between_dates(date1, date2)
+# Output: 1
 
-y2 = int(date2[:4])
-m2 = int(date2[5:7])
-d2 = int(date2[8:])
-
-days1 = day(y1, m1, d1)
-days2 = day(y2, m2, d2)
-
-return abs(days1-days2)
+date1 = "2020-01-15"
+date2 = "2019-12-31"
+puts days_between_dates(date1, date2)
+# Output: 15
