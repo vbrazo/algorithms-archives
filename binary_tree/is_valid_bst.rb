0cd9b031e9c7cf7837ec1b6902eab648cd854ffb
Vitor Oliveira <vbrazo@gmail.com>
2021-05-29 14:28:00 -0700

    Uncomment TreeNode

--- binary_tree/is_valid_bst.rb
+++ binary_tree/is_valid_bst.rb
@@ -1,12 +1,13 @@
 # Definition for a binary tree node.
-# class TreeNode
-#     attr_accessor :val, :left, :right
-#     def initialize(val = 0, left = nil, right = nil)
-#         @val = val
-#         @left = left
-#         @right = right
-#     end
-# end
+class TreeNode
+  attr_accessor :val, :left, :right
+
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
 
 #
 # Approach 1: Recursive Inorder Traversal
