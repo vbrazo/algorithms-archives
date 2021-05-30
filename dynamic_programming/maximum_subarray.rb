371e7026214561dec128db8ee2f7c88223fbd608
Vitor Oliveira <vbrazo@gmail.com>
2021-05-29 17:27:47 -0700

    Add Divide and Conquer (Advanced) approach

--- dynamic_programming/maximum_subarray.rb
+++ dynamic_programming/maximum_subarray.rb
@@ -65,3 +65,64 @@ def max_sub_array(nums)
 
   return max_so_far.max
 end
+
+#
+# Approach 3: Divide and Conquer (Advanced)
+#
+
+# Complexity Analysis
+#
+# Time complexity: O(N⋅log n), where N is the length of nums.
+# Space complexity: O(log n), where N is the length of nums.
+
+# @param {Integer[]} nums
+# @return {Integer}
+def max_sub_array(nums)
+  return nil if nums.empty?
+  return get_max_for_subarray(nums, 0, nums.length - 1)
+end
+
+def get_max_for_subarray(nums, left, right)
+  if left > right
+    return -Float::Infinity
+  elsif left == right
+    return nums[left]
+  end
+
+  mid = (left + right) / 2
+
+  # Get max of left half
+  left_max = get_max_for_subarray(nums, left, mid)
+
+  # Get max of right half
+  right_max = get_max_for_subarray(nums, mid + 1, right)
+
+  # Get the max value across the middle
+  mid_max = get_max_across_middle(nums, left, right)
+
+  return [left_max, right_max, mid_max].max
+end
+
+def get_max_across_middle(nums, left, right)
+  mid = (left + right) / 2
+
+  # Get max across left half, starting from mid
+  left_mid_max = -Float::INFINITY
+  sum_so_far = 0
+
+  mid.downto(left).each do |i|
+    sum_so_far += nums[i]
+    left_mid_max = [left_mid_max, sum_so_far].max
+  end
+
+  # Get max across right half, starting from mid + 1
+  right_mid_max = -Float::INFINITY
+  sum_so_far = 0
+
+  (mid+1).upto(right).each do |i|
+    sum_so_far += nums[i]
+    right_mid_max = [right_mid_max, sum_so_far].max
+  end
+
+  left_mid_max + right_mid_max
+end
