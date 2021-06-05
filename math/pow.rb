df1b4f8a45e5cb3f9e41b9a83ea1ea5a7b5ed2e2
Vitor Oliveira <vbrazo@gmail.com>
2021-06-04 19:10:53 -0700

    add fast power algorithm iterative approach

--- math/pow.rb
+++ math/pow.rb
@@ -46,3 +46,30 @@ def my_pow(x, n)
 
   n > 0 ? result : 1 / result
 end
+
+#
+# Approach 2: Fast Power Algorithm Iterative
+#
+
+# Complexity Analysis
+#
+# Time complexity: O(logn). For each bit of n 's binary representation, we will at most multiply once. So the total time complexity is O(logn).
+# Space complexity: O(1). We only need two variables for the current product and the final result of x.
+
+# O (log n) solution in Ruby, non-recursive
+
+def my_pow(x, n)
+  return 1 if n == 0
+
+  n1 = n < 0 ? -n : n
+  x1, res = x, 1
+
+  while true
+    res *= x1 if (n1 & 1 == 1)
+    x1 *= x1
+    n1 >>= 1
+    break if n1 == 0
+  end
+
+  return (n < 0) ? 1 / res : res
+end
