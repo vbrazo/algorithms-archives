8aa931986356a6687d6b4c28aa446b8b0f1562a2
Vitor Oliveira <vbrazo@gmail.com>
2021-06-05 13:03:04 -0700

    Add backtracking approach (without swapping)

---
+++ backtracking/permutation.rb
@@ -0,0 +1,58 @@
+# Given an array nums of distinct integers, return all the
+# possible permutations. You can return the answer in any
+# order.
+#
+# Example 1:
+#
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+#
+# Example 2:
+#
+# Input: nums = [0,1]
+# Output: [[0,1],[1,0]]
+#
+# Example 3:
+#
+# Input: nums = [1]
+# Output: [[1]]
+
+#
+# Approach 1: Backtracking (Without swapping)
+#
+
+def backtrack(nums, cur_array)
+  if(cur_array.size == nums.size)
+    @ans << cur_array.clone
+
+    return
+  end
+
+  (0..nums.length-1).each do |i|
+    next if cur_array.include?(nums[i])
+
+    cur_array << nums[i]
+
+    backtrack(nums, cur_array)
+
+    cur_array.pop
+  end
+end
+
+def permute(nums)
+  @ans = []
+  backtrack(nums, [])
+  @ans
+end
+
+nums = [1,2,3]
+permute(nums)
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+nums = [0,1]
+permute(nums)
+# Output: [[0,1],[1,0]]
+
+nums = [1]
+permute(nums)
+# Output: [[1]]
